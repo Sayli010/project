3a9a4640e5d3428ada7cbe080e831ba8a8bf24c8
Sayli010 <saylipisal6@gmail.com>
2019-11-28 12:50:18 +0530

    login module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule ,Routes} from '@angular/router'
 import { AppComponent } from './app.component';
+import { UserlayoutComponent } from './userlayout/userlayout.component';
+import { LoginComponent } from './userlayout/login/login.component';
+import { ProcessSelectionComponent } from './userlayout/process-selection/process-selection.component';
+import { from } from 'rxjs';
 
+const routes: Routes = [
+    { path: ' process-selection', component:  LoginComponent},
+    { path: ' ', component:  ProcessSelectionComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserlayoutComponent,
+    LoginComponent,
+    ProcessSelectionComponent,
+ 
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
