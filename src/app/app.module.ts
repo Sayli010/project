cf97ee97c63532d90b7a12d123e8649dd156f648
Sayli010 <saylipisal6@gmail.com>
2019-11-30 17:10:29 +0530

    nextCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { UserlayoutComponent } from './userlayout/userlayout.component';
 import { LoginComponent } from './userlayout/login/login.component';
 import { ProcessSelectionComponent } from './userlayout/process-selection/process-selection.component';
 import { from } from 'rxjs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 const routes: Routes = [
-    { path: ' process-selection', component:  LoginComponent},
-    { path: ' ', component:  ProcessSelectionComponent }
+    { path: ' ', component:  LoginComponent},
+    { path: 'process-selection', component:  ProcessSelectionComponent }
 ];
 @NgModule({
   declarations: [
@@ -17,12 +19,14 @@ const routes: Routes = [
     UserlayoutComponent,
     LoginComponent,
     ProcessSelectionComponent,
+   
  
     
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
